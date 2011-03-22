286b0a9b2ec4295d9dea19b00489a7e9570de751
Michal Janiszewski <janisozaur@gmail.com>
2011-03-22 20:31:42 +0100

    [zad1] dychotomia

---
+++ zad1/dychotomia.m
@@ -0,0 +1,18 @@
+function x = dychotomia(fun, a, b, epsilon)
+	middle = (a + b) / 2;
+	d = abs(b - middle) / 2;
+	xl = middle - d;
+	xp = middle + d;
+	if ((b - a) < epsilon)
+		if (valueInPoint(fun, xl) > valueInPoint(fun, xp))
+			x = xp;
+		else
+			x = xl;
+		end
+	elseif (valueInPoint(fun, xl) > valueInPoint(fun, xp))
+		x = dychotomia(fun, xl, b, epsilon);
+	else
+		x = dychotomia(fun, a, xp, epsilon);
+	end
+end
+
