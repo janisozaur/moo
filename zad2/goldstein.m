846c22b74f566d4e15c8ca41bab86a98a571b85d
Michal Janiszewski <janisozaur@gmail.com>
2011-04-02 00:50:03 +0200

    [zad2] add goldstein method

---
+++ zad2/goldstein.m
@@ -0,0 +1,17 @@
+function [result, iterations] = goldstein(f, startPoint, direction, c1, c2, ro, x, epsilon, iterationsInput)
+	iterations = iterationsInput + 1;
+	delta = -sign(myDiff(f, startPoint, direction, x, epsilon));
+	while (~goldsteinCondition(f, startPoint, direction, x, c1, delta, epsilon))
+		delta = ro * delta;
+	end
+	x = x + delta;
+	result = x;
+	if (abs(myDiff(f, startPoint, direction, x, epsilon)) > epsilon)
+		[result, iterations] = goldstein(f, startPoint, direction, c1, c2, ro, x, epsilon, iterations);
+	end
+end
+
+function result = goldsteinCondition(f, startPoint, direction, x, c, mystep, epsilon)
+	result = valueInPoint(f, startPoint, direction, x) + (1 - c) * mystep * myDiff(f, startPoint, direction, x, epsilon) <= valueInPoint(f, startPoint, direction, x + mystep);
+	result = result & (valueInPoint(f, startPoint, direction, x + mystep) <= c * mystep * myDiff(f, startPoint, direction, x, epsilon) + valueInPoint(f, startPoint, direction, x));
+end
