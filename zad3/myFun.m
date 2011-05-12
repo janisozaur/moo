ce7214f254c47f95376da633799c1b0ec724cda5
janisozaur <janisozaur@gmail.com>
2011-05-12 11:32:24 +0200

    [zad3] add test function from "applied optimization with matlab": http://www.wiley.com/legacy/products/subject/engineering/venkat/chapter6.html, example6_1

--- zad3/myFun.m
+++ zad3/myFun.m
@@ -1,5 +1,6 @@
 function res = myFun(a)
 	%res = sin(a(1)) + cos(a(2));
 	%res = (1-a(1))^2+100*(a(2)-a(1)^2)^2;
-	res = a(1)^2 + a(2)^2;
+	%res = a(1)^2 + a(2)^2;
+	res = 3 + (a(1) - 1.5*a(2))^2 + (a(2) - 2)^2;
 end
