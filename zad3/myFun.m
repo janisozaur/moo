01ae0a965f0d3699cee37a9af908c2759f5dea9b
janisozaur <janisozaur@gmail.com>
2011-05-12 10:44:58 +0200

    [zad3] add sample function

--- zad3/myFun.m
+++ zad3/myFun.m
@@ -1,4 +1,5 @@
 function res = myFun(a)
-	res = sin(a(1)) + cos(a(2));
-	%res = (1-a(1))^2+100.*(a(2)-a(1)^2)^2;
+	%res = sin(a(1)) + cos(a(2));
+	%res = (1-a(1))^2+100*(a(2)-a(1)^2)^2;
+	res = a(1)^2 + a(2)^2;
 end
