759e3c533ce78ff33b8522984f625cadf0ca33b2
janisozaur <janisozaur@gmail.com>
2011-05-12 11:33:15 +0200

    [zad3] lambda > 0 my ass. use -1..1 range to find lambda - this makes the dfp work

--- zad3/lambdaSelect.m
+++ zad3/lambdaSelect.m
@@ -1,8 +1,11 @@
 function lambda = lambdaSelect(targetFun, x, d)
 	y = inf;
 	lambda = 0.5;
-	for i = 1:100
-		yTemp = feval(targetFun, x - 0.01 * i * d);
+	for i = -100:100
+		if (i == 0)
+			continue;
+		end
+		yTemp = feval(targetFun, x + 0.01 * i * d);
 		if (yTemp < y)
 			y = yTemp;
 			lambda = i;
